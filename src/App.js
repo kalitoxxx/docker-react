76a195575f7711183fb408f08d0b4d90e7a1d784
Albert <kalitoxxx@gmail.com>
2019-11-11 17:59:07 -0300

    fix Dockerfile for beanstalk; change App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hi there 3
+          I was changed on the develop branch
         </p>
         <a
           className="App-link"
